ff387ed80ac21d7f3af0ecd412b409922b9ef8da
Sergey Kononov <dev.kononov@gmail.com>
2021-04-19 08:39:34 +0300

    mongoose

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 10001,
-    mongoURI: `mongodb://kononov:291091@127.0.0.1:27017, nft`,
+    mongoURI: `mongodb://kononov:291091@127.0.0.1:27017/picipo?authSource=admin`,
     auctionContract: '',
     baseAddress: ''
     // TG_BOT_ID: ''
